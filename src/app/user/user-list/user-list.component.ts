0a45e0d65b0c57864fc1f38436117636ba214d65
oscar <obravo@inserious.com>
2019-02-03 16:54:35 +0100

    eliminados test y añadidas animaciones básicas

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -4,27 +4,45 @@ import { Router } from '@angular/router';
 import { UserService } from '../shared/user.service';
 import { User } from '../shared/user.model';
 import { DialogComponent } from '../../shared/dialog/dialog.component';
+import { trigger, state, transition, animate, style } from '@angular/animations';
 
 @Component({
 
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
+  animations: [
+    trigger('changeDivSize', [
+      state('initial', style({
+        opacity: 0,
+        marginTop: '-50px'
+      })),
+      state('final', style({
+        opacity: 1,
+        marginTop: '0px'
+      })),
+      transition('initial=>final', animate('500ms')),
+      transition('final=>initial', animate('500ms'))
+    ])
+  ]
 })
 export class UserListComponent implements OnInit {
 
   public loading:boolean;
 	public haveError:boolean;
+  public currentState:string;
 	public displayedColumns:Array<string>;
 	public dataSource = new MatTableDataSource();
 
 	@ViewChild(MatSort) sort: MatSort;
+
 
   constructor(
 		private userService:UserService,
     private router:Router,
     private dialog:MatDialog
   ) {
+    this.currentState = 'initial';
 		this.loading = true;
 		this.haveError = false;
   }
@@ -39,6 +57,7 @@ export class UserListComponent implements OnInit {
         this.displayedColumns = ['id', 'name', 'birthdate'];
         this.dataSource = new MatTableDataSource(res.slice());
         this.dataSource.sort = this.sort;
+        this.currentState = 'final';
       }else{
         this.haveError = true;
       }
@@ -51,6 +70,11 @@ export class UserListComponent implements OnInit {
       this.loading = false;
     });
   }
+
+  public applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   public editUser = (userToEdit:User):void =>{
 		if(userToEdit.hasOwnProperty('id') && String(userToEdit.id).length > 0){
 			this.router.navigate ( [ '/user/edit', userToEdit.id ] );
